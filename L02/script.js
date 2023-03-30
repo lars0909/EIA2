5e75b141e4d2209fe5036e81e0721651724b3f59
Lars <lars.riehle@outlook.de>
2023-03-30 11:43:58 +0200

    btn

--- L02/script.js
+++ L02/script.js
@@ -1,2 +1,18 @@
-document.querySelectorAll();
+window.addEventListener(`load`, function handleLoad() {
+    document.addEventListener(`mousemove`, setInfoBox);
+    document.addEventListener(`click`, logInfo);
+    document.addEventListener(`key up`, logInfo);
+    document.body.addEventListener(`click`, logInfo);
+    document.body.addEventListener(`key up`, logInfo);
+    const divs = document.querySelectorAll("div");
+    divs.forEach(function addingListener(div) {
+        div.addEventListener(`click`, logInfo);
+        div.addEventListener(`key up`, logInfo);
+    });
+});
+function setInfoBox(_event) {
+}
+function logInfo(_event) {
+}
+;
 //# sourceMappingURL=script.js.map
--- L02/script.ts
+++ L02/script.ts
@@ -1,1 +1,21 @@
-document.querySelectorAll()
+window.addEventListener(`load`, function handleLoad(){
+    document.addEventListener(`mousemove`,setInfoBox )
+
+    document.addEventListener(`click`, logInfo);
+    document.addEventListener(`key up`, logInfo);
+
+    document.body.addEventListener(`click`, logInfo);
+    document.body.addEventListener(`key up`, logInfo);
+
+    const divs = document.querySelectorAll("div");
+    divs.forEach(function addingListener(div){
+        div.addEventListener(`click`, logInfo);
+        div.addEventListener(`key up`, logInfo);
+    })
+});
+function setInfoBox(_event: MouseEvent){
+
+}:
+function logInfo(_event: Event){
+
+};
