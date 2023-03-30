ab23098e8c086672bb59fe761f608f3afd28bb23
Lars <lars.riehle@outlook.de>
2023-03-30 13:03:36 +0200

    log

--- L02/script.js
+++ L02/script.js
@@ -1,3 +1,10 @@
+/*
+Aufgabe: L02_EventInspector
+Name: Lars Riehle
+Matrikel: 272305
+Datum: 30.03.2023
+Quellen:
+*/
 window.addEventListener(`load`, function handleLoad() {
     document.addEventListener(`mousemove`, setInfoBox);
     document.addEventListener(`click`, logInfo);
@@ -9,10 +16,26 @@ window.addEventListener(`load`, function handleLoad() {
         div.addEventListener(`click`, logInfo);
         div.addEventListener(`key up`, logInfo);
     });
+    // console.log("listeners added");
 });
 function setInfoBox(_event) {
+    const mouseX = _event.clientX;
+    const mouseY = _event.clientY;
+    // console.log(mouseX);
+    // console.log(mouseY);
+    let x = String(mouseX + 20);
+    let y = String(mouseY + 20);
+    const infobox = document.getElementById("infobox");
+    infobox.innerHTML = "Mouse x: " + x + " Mouse y: " + y + "Event-Target: " + _event.target;
+    // console.log("content wird angezeigt");
+    infobox.style.position = "fixed";
+    infobox.style.top = y + "px";
+    infobox.style.left = x + "px";
+    // console.log("infoBox wird an Mouse geheftet");
 }
+;
 function logInfo(_event) {
+    console.log(_event.type + _event.target + _event.currentTarget + _event);
 }
 ;
 //# sourceMappingURL=script.js.map
--- L02/script.ts
+++ L02/script.ts
@@ -1,5 +1,12 @@
-window.addEventListener(`load`, function handleLoad(){
-    document.addEventListener(`mousemove`,setInfoBox )
+/*
+Aufgabe: L02_EventInspector
+Name: Lars Riehle
+Matrikel: 272305
+Datum: 30.03.2023
+Quellen: 
+*/
+window.addEventListener(`load`, function handleLoad() {
+    document.addEventListener(`mousemove`, setInfoBox)
 
     document.addEventListener(`click`, logInfo);
     document.addEventListener(`key up`, logInfo);
@@ -8,14 +15,28 @@ window.addEventListener(`load`, function handleLoad(){
     document.body.addEventListener(`key up`, logInfo);
 
     const divs = document.querySelectorAll("div");
-    divs.forEach(function addingListener(div){
+    divs.forEach(function addingListener(div) {
         div.addEventListener(`click`, logInfo);
         div.addEventListener(`key up`, logInfo);
     })
+    // console.log("listeners added");
+
 });
-function setInfoBox(_event: MouseEvent){
-
-}:
-function logInfo(_event: Event){
-
+function setInfoBox(_event: MouseEvent) {
+    const mouseX: number = _event.clientX;
+    const mouseY: number = _event.clientY;
+    // console.log(mouseX);
+    // console.log(mouseY);
+    let x: string = String(mouseX + 20);
+    let y: string = String(mouseY + 20);
+    const infobox = document.getElementById("infobox");
+    infobox.innerHTML = "Mouse x: " + x + " Mouse y: " + y + "Event-Target: " + _event.target;
+    // console.log("content wird angezeigt");
+    infobox.style.position = "fixed";
+    infobox.style.top = y+ "px";
+    infobox.style.left = x + "px";
+    // console.log("infoBox wird an Mouse geheftet");
+};
+function logInfo(_event: Event) {
+    console.log(_event.type + _event.target + _event.currentTarget + _event);
 };
