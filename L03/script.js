d0ad4484be7ef41daa871d6cfe2c50e9972aec5a
Lars <lars.riehle@outlook.de>
2023-04-15 17:33:40 +0200

    uff

--- L03/script.js
+++ L03/script.js
@@ -1,11 +1,48 @@
 /*
-Aufgabe: L03_Aufgabenliste_Formular
+Aufgabe: L03_Aufgabenliste_DAtenstruktur
 Name: Lars Riehle
 Matrikel: 272305
 Datum: 13.04.2023
 Quellen:
 */
-document.querySelector("#addTaskBtn").addEventListener(`click`, function test() {
-    console.log("der Knopf wurde gedrückt");
+const inputTask = document.querySelector("#inputTask");
+const inputName = document.querySelector("#inputPerson");
+const inputDate = document.querySelector("#inputDate");
+const inputInfo = document.querySelector("#inputInfo");
+const addTaskBtn = document.querySelector("#addTaskBtn");
+const inputs = document.querySelectorAll("input");
+addTaskBtn.addEventListener(`click`, function () {
+    addTask();
 });
+function addTask() {
+    let Task = document.createElement("li");
+    let task = document.createElement("h1");
+    task.innerHTML = inputTask.value;
+    let name = document.createElement("p");
+    name.innerHTML = inputName.value;
+    let date = document.createElement("p");
+    date.innerHTML = inputDate.value;
+    let info = document.createElement("p");
+    info.innerHTML = inputInfo.value;
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "delete";
+    deleteBtn.addEventListener(`click`, function () {
+        deleteTask(Task);
+    });
+    document.querySelector("#TaskList").appendChild(Task);
+    Task.appendChild(task);
+    Task.appendChild(name);
+    Task.appendChild(date);
+    Task.appendChild(info);
+    Task.appendChild(deleteBtn);
+    clearInput();
+}
+function clearInput() {
+    inputs.forEach(function (input) {
+        input.value = "";
+    });
+}
+function deleteTask(Task) {
+    Task.remove();
+}
 //# sourceMappingURL=script.js.map
--- L03/script.ts
+++ L03/script.ts
@@ -1,10 +1,55 @@
 /*
-Aufgabe: L03_Aufgabenliste_Formular
+Aufgabe: L03_Aufgabenliste_DAtenstruktur
 Name: Lars Riehle
 Matrikel: 272305
 Datum: 13.04.2023
 Quellen: 
 */
-document.querySelector("#addTaskBtn").addEventListener(`click`, function test(){
-    console.log("der Knopf wurde gedrückt");
+const inputTask = document.querySelector("#inputTask") as HTMLInputElement;
+const inputName = document.querySelector("#inputPerson") as HTMLSelectElement;
+const inputDate = document.querySelector("#inputDate") as HTMLInputElement;
+const inputInfo = document.querySelector("#inputInfo") as HTMLInputElement;
+const addTaskBtn = document.querySelector("#addTaskBtn") as HTMLButtonElement;
+
+const inputs = document.querySelectorAll("input");
+
+addTaskBtn.addEventListener(`click`, function(){
+    addTask();
 })
+
+function addTask (){
+    let Task: HTMLLIElement = document.createElement("li");
+    let task = document.createElement("h1");
+    task.innerHTML = inputTask.value;
+    let name: HTMLParagraphElement = document.createElement("p");
+    name.innerHTML = inputName.value;
+    let date: HTMLParagraphElement = document.createElement("p");
+    date.innerHTML = inputDate.value;
+    let info: HTMLParagraphElement = document.createElement("p");
+    info.innerHTML = inputInfo.value;
+    let deleteBtn: HTMLButtonElement = document.createElement("button");
+    deleteBtn.innerHTML = "delete";
+    deleteBtn.addEventListener(`click`, function(){
+        deleteTask(Task);
+    })
+
+    document.querySelector("#TaskList").appendChild(Task);
+
+    Task.appendChild(task);
+    Task.appendChild(name);
+    Task.appendChild(date);
+    Task.appendChild(info);
+    Task.appendChild(deleteBtn);
+    
+    clearInput();
+}
+
+function clearInput(){
+    inputs.forEach(function(input: HTMLInputElement) {
+        input.value = "";
+    })
+}
+
+function deleteTask(Task: HTMLLIElement){
+    Task.remove();
+}
